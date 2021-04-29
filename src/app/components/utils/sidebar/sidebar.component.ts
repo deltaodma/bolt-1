d231dca8a6a6c860c9b435bd86904dca3fa1654c
cybarreto <cybarreto@indracompany.com>
2021-04-29 15:29:08 -0500

    sidebar styles fixed

--- src/app/components/utils/sidebar/sidebar.component.ts
+++ src/app/components/utils/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core'
 import { MatAccordion } from '@angular/material/expansion'
+import { MockProjects } from 'src/app/mocks/projects-mock'
 
 @Component({
   selector: 'app-sidebar',
@@ -17,66 +18,8 @@ export class SidebarComponent implements OnInit {
   public favList: any = [1, 2, 3, 4]
   public role: string = 'user'
   public sideStatus: boolean = false
+  public prop = MockProjects
 
-  prop = [
-    {
-      name: 'bolt',
-      menu: [
-        {
-          name: 'user',
-          prog: [
-            { item: '1', fav: false },
-            { item: '2', fav: false },
-            { item: '3', fav: true },
-            { item: '4', fav: false },
-          ],
-        },
-        {
-          name: 'exter',
-          prog: [
-            { item: '5', fav: false },
-            { item: '6', fav: false },
-          ],
-        },
-        {
-          name: 'qa',
-          prog: [
-            { item: '7', fav: false },
-            { item: '8', fav: true },
-            { item: '9', fav: false },
-          ],
-        },
-      ],
-    },
-    {
-      name: 'becks',
-      menu: [
-        {
-          name: 'user2',
-          prog: [
-            { item: '10', fav: true },
-            { item: '11', fav: false },
-          ],
-        },
-        { name: 'exter2', prog: [{ item: '12', fav: false }] },
-        { name: 'qa2', prog: [{ item: 'asdasdas', fav: true }] },
-      ],
-    },
-    {
-      name: 'pony malta',
-      menu: [
-        {
-          name: 'user3',
-          prog: [
-            { item: '10', fav: true },
-            { item: '11', fav: false },
-          ],
-        },
-        { name: 'exter3', prog: [{ item: '12', fav: true }] },
-        { name: 'q3', prog: [{ item: 'asdasdas', fav: false }] },
-      ],
-    },
-  ]
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/mocks/projects-mock.ts
@@ -0,0 +1,59 @@
+export const MockProjects = [
+  {
+    name: 'bolt',
+    menu: [
+      {
+        name: 'user',
+        prog: [
+          { item: '1', fav: false },
+          { item: '2', fav: false },
+          { item: '3', fav: true },
+          { item: '4', fav: false },
+        ],
+      },
+      {
+        name: 'exter',
+        prog: [
+          { item: '5', fav: false },
+          { item: '6', fav: false },
+        ],
+      },
+      {
+        name: 'qa',
+        prog: [
+          { item: '7', fav: false },
+          { item: '8', fav: true },
+          { item: '9', fav: false },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'becks',
+    menu: [
+      {
+        name: 'user2',
+        prog: [
+          { item: '10', fav: true },
+          { item: '11', fav: false },
+        ],
+      },
+      { name: 'exter2', prog: [{ item: '12', fav: false }] },
+      { name: 'qa2', prog: [{ item: 'asdasdas', fav: true }] },
+    ],
+  },
+  {
+    name: 'pony malta',
+    menu: [
+      {
+        name: 'user3',
+        prog: [
+          { item: '10', fav: true },
+          { item: '11', fav: false },
+        ],
+      },
+      { name: 'exter3', prog: [{ item: '12', fav: true }] },
+      { name: 'q3', prog: [{ item: 'asdasdas', fav: false }] },
+    ],
+  },
+]
