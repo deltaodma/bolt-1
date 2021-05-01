25614ded98b465d7e7e61fbcf2f110b981dbc00e
cybarreto <cybarreto@indracompany.com>
2021-05-01 11:21:01 -0500

    carousel finished fav cards added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { UtilsModule } from './components/utils/utils.module';
+import { UtilsModule } from './components/utils/utils.module'
 import { HomeComponent } from './components/home/home/home.component'
+
+import { MatCarouselModule } from '@ngmodule/material-carousel'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -14,6 +17,8 @@ import { HomeComponent } from './components/home/home/home.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     UtilsModule,
+    MatCarouselModule.forRoot(),
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public date = new Date().toLocaleDateString()
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  redirectTo() {
+    console.log('redireccion externa')
   }
-
 }
