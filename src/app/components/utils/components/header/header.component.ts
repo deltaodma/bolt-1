9f410c7f7c3570853048fb1d10355199e30f105e
cybarreto <cybarreto@indracompany.com>
2021-05-27 08:23:11 -0500

    select header manched for mat-select banners card min-width added

--- src/app/components/utils/components/header/header.component.ts
+++ src/app/components/utils/components/header/header.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http'
 import { AfterContentChecked, Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 
-
 type ProfileType = {
   givenName?: string
   surname?: string
@@ -19,24 +18,19 @@ export class HeaderComponent implements OnInit {
   public lang: string
   profile!: ProfileType
   public userName: string = 'Cristhopher'
-  constructor(
-    private router: Router,
-    private http: HttpClient,
-  ) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.lang = localStorage.getItem('lang') || 'Esp'
-
   }
 
   changeLang(event) {
     // read the local storage to set a language
-    localStorage.setItem('lang', event)
+    localStorage.setItem('lang', event.value)
     window.location.reload()
   }
 
   routerHome() {
     this.router.navigate(['home'])
   }
-
 }
