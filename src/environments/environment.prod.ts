17736a677eac0a364f767da33742c482dc372c28
cybarreto <cybarreto@indracompany.com>
2021-06-01 11:40:22 -0500

    environment prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   serverUrl: 'https://bolt-back.herokuapp.com/api/v1/',
+
+  auth: {
+    get: 'auth/saml',
+  },
+
   login: {
     resource: '/oauth/token?_format=json',
   },
@@ -10,23 +15,52 @@ export const environment = {
   banners: {
     getAll: 'banners',
     post: 'banners',
-    upload: 'upload',
+    upload: 'banners/changestatus/',
     getById: 'banners/',
     putById: 'banners/',
     deleteById: 'banners/',
   },
   projects: {
-    getAll: 'projects',
+    get: 'projects',
+    getAll: 'projects/menu',
     post: 'projects',
     getById: 'projects/',
     putById: 'projects/',
+    updateStatusById: 'projects/changestatus/',
     deleteById: 'projects/',
   },
   submenus: {
-    getAll: 'apps',
+    get: 'submenus',
+    post: 'submenus',
+    getById: 'submenus/',
+    putById: 'submenus/',
+    deleteById: 'submenus/',
+    updateStatusById: 'submenus/changestatus/',
+  },
+  apps: {
+    get: 'apps',
+    getAssoc: 'apps/assoc',
     post: 'apps',
     getById: 'apps/',
     putById: 'apps/',
     deleteById: 'apps/',
+    updateStatusById: 'apps/changestatus/',
+  },
+  users: {
+    getAll: 'user',
+    getSaml: 'user/saml',
+    getById: 'user/',
+    post: 'user',
+    putById: 'user/',
+    deleteById: 'user/',
+    updateStatusById: 'user/changestatus/',
+  },
+  roles: {
+    getAll: 'roles',
+    getById: 'roles/',
+    post: 'roles',
+    putById: 'roles/',
+    deleteById: 'roles/',
+    updateStatusById: 'roles/changestatus/',
   },
 }
